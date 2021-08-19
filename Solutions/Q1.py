e30ef391f4e351171fed5602f867ce0102e05468
KimtVak8143 <mk8143@srmist.edu.in>
2021-08-19 15:47:57 +0530

    added Q1

--- Solutions/Q1.py
+++ Solutions/Q1.py
@@ -1,2 +1,19 @@
 # Solution of Problem 1
 
+def swap_case(str1):
+    new = []
+    for x in str1:
+        if x.isupper():
+            new.append(x.lower())
+        elif x.islower():
+            new.append(x.upper())
+        else:
+            new.append(x)
+    ret = ""
+    return ret.join(new)
+
+
+if __name__ == '__main__':
+    s = input("Enter String :")
+    result = swap_case(s)
+    print(result)
