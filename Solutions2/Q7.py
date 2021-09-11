2ef552bbf809dc3d9e098ea80d785680761c2b79
KimtVak8143 <mk8143@srmist.edu.in>
2021-09-11 19:57:45 +0530

    capitalize

--- Solutions2/Q7.py
+++ Solutions2/Q7.py
@@ -1,15 +1,31 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        new = {}
-        flag = 0
-        for a in nums:
-            try:
-                new[a] += 1
-            except:
-                new[a] = 1
-        for a in new:
-            if new[a] > 1:
-                return True
-        else:
-            return False
-        
+# Capitalize
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+# Complete the solve function below.
+
+
+def solve(s):
+    # # Approach 1
+    # return s.title()
+
+    # # Approach 2
+    # new = ' '.join(map(str.capitalize, s.split(' ')))
+    # return new
+
+    # Approach 3
+    for x in s[:].split():
+        s = s.replace(x, x.capitalize())
+    return s
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    fptr.close()
